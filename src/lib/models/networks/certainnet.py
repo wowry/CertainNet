Fix Lreg calculation

src/lib/models/networks/certainnet.py
@@ -511,8 +511,9 @@ class CertainNet(nn.Module):
 
         return diff, y_mapped
     
-    def calc_L_reg(self, y, y_mapped):
+    def calc_Lreg(self, y, y_mapped):
         Lambda = self.Lambda
+        sigma = self.length_scale
 
         y = y.permute(1, 0, 2, 3).reshape(y.size()[1], -1)
         y_mapped = y_mapped.permute(2, 0, 1)
@@ -531,7 +532,8 @@ class CertainNet(nn.Module):
             prod_sum += prod.sum()
             y_sum += y_lambda.sum()
         
-        reg_loss = prod_sum / y_sum
+        denom = torch.clamp(y_sum, min=1) / (sigma ** 2)
+        reg_loss = prod_sum / denom
         return reg_loss
     
     def update_embeddings(self, x, y):

src/lib/trains/base_trainer.py
@@ -20,7 +20,7 @@ class ModelWithLoss(torch.nn.Module):
   def forward(self, batch):
     if 'certainnet' in self.arch:
       outputs, y_mapped = self.model(batch['input'])
-      reg_loss = self.model.calc_L_reg(batch['hm'], y_mapped)
+      reg_loss = self.model.calc_Lreg(batch['hm'], y_mapped)
       loss, loss_stats = self.loss(outputs, batch, reg_loss, self.reg_weight)
     else:
       outputs = self.model(batch['input'])